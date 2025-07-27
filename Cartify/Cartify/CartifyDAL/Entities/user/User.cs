51f7a454b988d01dc2b40c02f471e95ec95c77f4
ziadelsayd <ziadelsayedx@gmail.com>
2025-07-27 23:50:40 +0300

    Fix Not found problem

---
+++ Cartify/Cartify/CartifyDAL/Entities/user/User.cs
@@ -0,0 +1,43 @@
+﻿using CartifyDAL.Entities.order;
+using System.ComponentModel.DataAnnotations;
+
+namespace CartifyDAL.Entities.user
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; private set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FName { get; private set; }
+
+        [MaxLength(100)]
+        public string LName { get; private set; }
+        public bool IsActive { get; private set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Type { get; private set; }
+
+        public DateTime? LastLogin { get; private set; }
+
+        // Navigation Properties
+        public List<UserPayment> UserPayments { get; private set; }
+        public List<UserAddress> Addresses { get; private set; }
+        public List<Order> Orders { get; private set; }
+
+        // Audit Fields
+        [Required]
+        public string CreatedBy { get; private set; }
+        public DateTime CreatedOn { get; private set; }
+        public string? ModifiedBy { get; private set; }
+        public DateTime? ModifiedOn { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public DateTime? DeletedOn { get; private set; }
+        public string? DeletedBy { get; private set; }
+
+
+    }
+
+}
---
+++ Cartify/Cartify/CartifyPLL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CartifyPLL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
