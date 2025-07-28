6b2e78e066a7450922334e0be832ac73e34f5038
omarmoashraf <omarmoashraf05@gmail.com>
2025-07-28 18:08:15 +0300

    added  CRUD operations for Product and Category

--- Cartify/Cartify/CartifyBLL/obj/Debug/net8.0/CartifyBLL.AssemblyInfo.cs
+++ Cartify/Cartify/CartifyBLL/obj/Debug/net8.0/CartifyBLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CartifyBLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d20964a510b3ecc5a5a74beeb2098ef2a7aece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e277d36a49cad726009f890bc4559c90baa646de")]
 [assembly: System.Reflection.AssemblyProductAttribute("CartifyBLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CartifyBLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Cartify/Cartify/CartifyDAL/Repo/categoryRepo/Abstraction/ICategoryRepo.cs
@@ -0,0 +1,14 @@
+﻿using CartifyDAL.Entities.category;
+
+namespace CartifyDAL.Repo.categoryRepo.Abstraction
+{
+    public interface ICategoryRepo
+    {
+
+        (bool, string?) Create(Category category);
+        (List<Category>,string?) GetAll();
+        (Category,string?) GetById(int categoryId);
+        (bool,string?) Update(Category category);
+        (bool, string?) Delete(int categoryId);
+    }
+}
