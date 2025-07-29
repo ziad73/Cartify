9a5da6cf80d98526ec0393d718ddd1f875cfda52
Hassan <hass23632@gmail.com>
2025-07-30 00:13:01 +0300

    Added CRUD cart, cartItem and ProductCart in Repo

---
+++ Cartify/Cartify/CartifyDAL/Repo/CartRepo/Abstraction/ICartRepo.cs
@@ -0,0 +1,15 @@
+﻿
+using CartifyDAL.Entities.cart;
+using CartifyDAL.Entities.category;
+
+namespace CartifyDAL.Repo.CartRepo.Abstraction
+{
+    public interface ICartRepo
+    {
+        (bool, string?) Create(Cart cart);
+        (List<Cart>, string?) GetAll();
+        (Cart, string?) GetById(int cartId);
+        (bool, string?) Update(Cart cart);
+        (bool, string?) Delete(int cartId);
+    }
+}
